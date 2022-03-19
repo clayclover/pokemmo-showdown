56b5ae68e5c6931f2abdb81000b35273b6add660
JeanMarc <tacticianjean@gmail.com>
2022-03-18 20:31:42 -0400

    Fixed Chansey/Blissey not being able to have Teleport+Wish

--- data/mods/mmo/learnsets.ts
+++ data/mods/mmo/learnsets.ts
@@ -15161,7 +15161,7 @@ export const Learnsets: {[speciesid: string]: LearnsetData} = {
 			watergun: ["7V"],
 			waterpulse: ["7T", "6T", "4M", "3M"],
 			wildcharge: ["8M", "7M", "6M", "5M"],
-			wish: ["3S0"],
+			wish: ["8M", "3S0"],
 			workup: ["8M", "7M", "5M"],
 			zapcannon: ["7V"],
 			zenheadbutt: ["8M", "7T", "6T", "5T", "4T"],
